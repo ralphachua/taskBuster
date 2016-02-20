bb16395a6cbfdc324630d62e14cb60468a8c5a37
Bien Mandac <blmandac@voyagerinnovation.com>
2016-02-20 23:36:18 +0800

    Consolidated pages into single module

---
+++ front_end/src/js/pages.js
@@ -0,0 +1,9 @@
+define([
+  'common/components/MyTasksPage/MyTasksPage',
+  'common/components/ProjectDetailsPage/ProjectDetailsPage'
+], function (MyTasks, ProjectDetails) {
+  return {
+    'my-tasks-page' : MyTasks,
+    'project-details-page' : ProjectDetails
+  };
+});
